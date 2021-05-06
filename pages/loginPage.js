0e22af3626e9f9069c565efd114e740c76453db5
Dominik Knapo <dominik.knapo@dixonscarphone.com>
2021-05-06 15:17:23 +0200

    Workshop

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -2,7 +2,30 @@ const { I } = inject();
 
 class LoginPage {
 
-    // your code here
+    form = locate('#login');
+
+    inputs = {
+        login: locate(this.form).find('#username'),
+        pass: locate(this.form).find('#password')
+    };
+
+    buttons = {
+        submit: locate(this.form).find('button[type=submit]')
+    };
+
+    fillInCorrectCredentials(login, pass) {
+        I.fillField(this.inputs.login, login);
+        I.fillField(this.inputs.pass, pass);
+    }
+
+    submitForm() {
+        I.click(this.buttons.submit);
+    }
+
+    sendFilledForm(login, pass) {
+        this.fillInCorrectCredentials(login, pass);
+        this.submitForm();
+    }
 
 }
 
