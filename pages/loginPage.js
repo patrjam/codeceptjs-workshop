db2c22b7310699e9e4cf56024782b698fddb63ce
LEHNEF01 <filip.lehnert01@dixonscarphone.com>
2021-08-13 11:32:47 +0200

    LoginPage Scenario

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -2,8 +2,29 @@ const { I } = inject();
 
 class LoginPage {
 
-    // your code here
 
+    buttons = {
+        submit: locate(this.form).find('button[type=submit]')
+    };
+
+    inputs = {
+        login: locate(this.form).find('#username'),
+        pass: locate(this.form).find('#password')
+    };
+
+    form = locate('#login page');
+
+
+    fillInCredentials(login, pass) {
+        I.fillField(this.inputs.login, login);
+        I.fillField(this.inputs.pass, pass);
+    }
+
+
+    fillInCredentialsAndLogin(login, pass) {
+        this.fillInCredentials(login, pass);
+        I.click(this.buttons.submit);
+    }
 }
 
 module.exports = new LoginPage();
